7e9cf3fc1f127ab3ff946772b8b987da85d18509
Nitiferi <arianitfer@gmail.com>
2014-12-16 23:09:03 +0100

    new

--- library/src/main/java/com/niti/fotobesa/lib/views/FotoBesaLayout.java
+++ library/src/main/java/com/niti/fotobesa/lib/views/FotoBesaLayout.java
@@ -86,7 +86,7 @@ public class FotoBesaLayout extends LinearLayout implements View.OnTouchListener
     }
 
     private void init(Context context) {
-        LayoutInflater.from(context).inflate(R.layout.listbuddies, this, true);
+        LayoutInflater.from(context).inflate(R.layout.fotobesa, this, true);
         mListViewLeft = (ObservableListView) findViewById(R.id.list_left);
         mListViewRight = (ObservableListView) findViewById(R.id.list_right);
         mLastViewTouchId = mListViewRight.getId();
